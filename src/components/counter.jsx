f7b6d07bb481e0cbe95d667a606f04076843a095
Casey Fayard <fayardcasey@gmail.com>
2020-08-09 12:50:19 -0400

    pushing files to repo, embedding expressions

---
+++ src/components/counter.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    return (
+      <>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
