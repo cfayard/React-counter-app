5669f469efae34f60491088c543a37fc799494fc
Casey Fayard <fayardcasey@gmail.com>
2020-08-09 16:18:52 -0400

    updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
   };
-  handleDecrement = () => {
-    this.setState({ value: this.state.value - 1 });
-  };
+  // handleDelete = () => {
+  //   this.setState({ value: this.state.value - 1 });
+  // };
 
   render() {
     return (
@@ -23,10 +23,10 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={() => this.handleDecrement()}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className={"btn btn-danger btn-sm m-2"}
         >
-          Decrement
+          Delete
         </button>
       </div>
     );
